1b652b7ea62036e7c076001f786833ce97f39298
Worysz <woryszlukasz@gmail.com>
2024-01-22 16:54:07 +0100

    Change marker to fre parking func

--- ipz_parkinghunter/lib/Pages/MainPage_view.dart
+++ ipz_parkinghunter/lib/Pages/MainPage_view.dart
@@ -105,7 +105,6 @@ class _MainPageState extends State<MainPage> {
   }
 }
 
-
   void _showErrorSnackBar(String message) {
     final snackBar = SnackBar(
       content: Text(message),
@@ -455,6 +454,31 @@ class _MainPageState extends State<MainPage> {
     // Obsłuż błąd, np. pokazując domyślny znacznik lub rejestrując błąd
   }
 }
+void changeMarkerToFreeParking() async {
+  if (_selectedMarkerId != null) {
+    // Pobierz aktualne dane znacznika
+    database.child(_selectedMarkerId!).once().then((DatabaseEvent event) {
+      if (event.snapshot.value != null) {
+        // Bezpieczne rzutowanie na Map<dynamic, dynamic>
+        Map<dynamic, dynamic> markerData = event.snapshot.value as Map<dynamic, dynamic>;
+        LatLng position = LatLng(markerData['latitude'], markerData['longitude']);
+
+        // Aktualizuj znacznik w Firebase
+        database.child(_selectedMarkerId!).update({
+          'isFreeParking': true,
+        }).then((_) {
+          // Usuń stary znacznik i dodaj nowy z aktualizacją
+          _markers.removeWhere((marker) => marker.markerId.value == _selectedMarkerId);
+          addFreeParkingMarker(_markers, position, _selectedMarkerId!);
+          setState(() {});
+        });
+      }
+    }).catchError((error) {
+      print('Error updating marker: $error');
+      _showErrorSnackBar('Failed to update marker');
+    });
+  }
+}
 
 
 Widget _buildFloatingActionButtons(bool showFullscreenButton) {
@@ -462,8 +486,17 @@ Widget _buildFloatingActionButtons(bool showFullscreenButton) {
     mainAxisAlignment: MainAxisAlignment.end,
     children: [
       if (_isFullscreen) _buildMinimizeButton(),
+
+      // Przycisk do zmiany stanu znacznika
+      if (_selectedMarkerId != null) _buildChangeToFreeParkingButton(),
+
+      // Przycisk do dodawania wolnego miejsca parkingowego
       _buildToggleFreeParkingButton(),
+
+      // SpeedDial z dodatkowymi opcjami
       _buildSpeedDial(),
+
+      // Przycisk do przełączania trybu pełnoekranowego
       if (showFullscreenButton) ...[
         SizedBox(height: 20),
         _buildFullscreenButton(),
